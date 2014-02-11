9a3efe8e6d98d9aa3b10c207da981e2cf108d6db
IOStudent <anna.veronica@gmail.com>
2014-02-11 08:24:52 -0500

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Anca Negrean on 2/11/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git Test/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  Git Test
+//
+//  Created by Anca Negrean on 2/11/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
