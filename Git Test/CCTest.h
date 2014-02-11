b004cabe3167a66dc6671b14112cef07b116ac5d
IOStudent <anna.veronica@gmail.com>
2014-02-11 12:09:39 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Anca Negrean on 2/11/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Anca Negrean on 2/11/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
